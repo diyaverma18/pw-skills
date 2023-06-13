06c7a0e9dc45c10211c3241982d1c3757c4f0a0d
diyaverma18 <112193669+diyaverma18@users.noreply.github.com>
2023-06-14 00:50:33 +0530

    Add files via upload
    assignment of pwskills

---
+++ ACSII.java
@@ -0,0 +1,6 @@
+public class ACSII {
+    public static void main(String[] args) {
+        int x='U';
+        System.out.println("The ASCII value of U IS:"+x);
+    }
+}
---
+++ area.java
@@ -0,0 +1,12 @@
+public class area {
+    public static void main(String[] args) {
+        int length=7;
+        int breadth=4;
+        System.out.println("The length is:"+length);
+        System.out.println("The breadth is:"+breadth);
+        int area=length*breadth;
+        System.out.println("The area is:"+area);
+
+
+    }
+}
